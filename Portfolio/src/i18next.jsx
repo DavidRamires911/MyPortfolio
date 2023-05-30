c80572eb95cbfb532be1e101e7292a12781b6d8a
DavidCorreia911 <davidcorreia911@gmail.com>
2023-05-30 22:59:54 +0100

    try5

--- Portfolio/src/i18next.jsx
+++ Portfolio/src/i18next.jsx
@@ -1,5 +1,3 @@
-
-
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend";
@@ -20,9 +18,7 @@ i18n
     },
     backend: {
       loadPath: "/locales/{{lng}}/{{ns}}.json",
-      // Set the baseURL option if your app is not deployed at the root of your domain
-      // e.g., if deployed at https://example.com/my-app, set baseURL: "/my-app"
-      baseURL: "https://davidcorreia.netlify.app/",
+      baseURL: "/",
     },
   });
 
