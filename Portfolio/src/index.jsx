4af64c5717468cd8e45ee375e8a2fa0fcf9ed639
DavidCorreia911 <davidcorreia911@gmail.com>
2023-05-31 23:36:02 +0100

    t1

--- Portfolio/src/index.jsx
+++ Portfolio/src/index.jsx
@@ -7,14 +7,16 @@ import { ThemeProvider } from './ThemeContext';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import "./services/i18n";
+
 ReactDOM.render(
-  <Suspense fallback={<div>Loading~~</div>}>
-    <I18nextProvider i18n={i18n}>
+  <I18nextProvider i18n={i18n}>
       <ThemeProvider>
+  <Suspense fallback={<div>Loading~~</div>}>
         <App />
+  </Suspense>,
       </ThemeProvider>
     </I18nextProvider>
-  </Suspense>,
   document.getElementById('root')
 );
 
