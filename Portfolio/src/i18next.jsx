8f9263dbd51fa4b8c88e190747180d1e74c7c35f
DavidCorreia911 <davidcorreia911@gmail.com>
2023-05-31 15:07:42 +0100

    ty

--- Portfolio/src/i18next.jsx
+++ Portfolio/src/i18next.jsx
@@ -3,7 +3,7 @@ import { initReactI18next } from "react-i18next";
 import Backend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-const Languages = ["en", "pt", "pt-PT"];
+const Languages = ["en", "pt"];
 
 i18n
   .use(Backend)
